7c3c3f27098937408cb40c36e542c9e5053264b0
Junobee25 <wnsdhqo1983@gmail.com>
2022-11-17 14:14:32 +0900

    final clone

--- Quest03/gallery-start/main.js
+++ Quest03/gallery-start/main.js
@@ -81,19 +81,34 @@ function OneClick5() {
 };
 DisplayList[1].classList.toggle('full-image') */
 
-function ClickEvent() {
-  /*for(var i =1; i<=5; i++){
-    displayed_img[i].addEventListener('click',function(){
-      displayed_img[0].setAttribute('src','images/pic' + i +'.jpg')
-    })
-  };*/
-  displayed_img[1].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic1.jpg') });
-  displayed_img[2].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic2.jpg') });
-  displayed_img[3].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic3.jpg') });
-  displayed_img[4].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic4.jpg'); });
-  displayed_img[5].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic5.jpg'); });
+
+/*
+  for(var i = 1; i<=5; i++){
+  displayed_img[i].addEventListener('click', function () {displayed_img[0].setAttribute('src', 'images/pic' + (i) + '.jpg'
+  https://blog.outsider.ne.kr/506
+
+  첫번째 예제는 클로저의 생성으로 인한 부작용
+원래 의도는 각 버튼마다 alert시에 1,2,3,4,5를 결과로 보여주려는 의도이나 이벤트 핸들러 함수의 i값이 바깥쪽 변수인 i값에 대한 참조를 유지하고 있어, 즉 클로저의 생성으로 인해 최종값인 5를 모두 가리키게 되는 예제입니다.
+사실 두번째 예제는 클로저의 부작용을 막기위한 처리로 제시한 예제인데, 이 예제도 클로저가 생성되긴 합니다만 익명함수의 인자로 값을 넘겨버림으로써 바깥쪽 변수인 i에 대한 변수스코프를 끊어버리고, 이벤트 핸들러에서는 익명함수의 인자값에 접근함으로써 의도한 대로 처리가 되게 됩니다.
+괄호로 둘러싼 함수표현식 안에서는 바깥쪽 변수에 접근하지 못한다는 것을 여기서 아실 수 있습니다.
+}*/
+
+for (var i = 1; i <= 5; i++) {
+  (function (m) {displayed_img[m].addEventListener('click', function () {displayed_img[0].setAttribute('src', 'images/pic' + (m) + '.jpg')});})(i);
+
 };
-ClickEvent();
+
+
+
+// displayed_img[1].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic1.jpg') });
+// displayed_img[2].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic2.jpg') });
+// displayed_img[3].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic3.jpg') });
+// displayed_img[4].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic4.jpg'); });
+// displayed_img[5].addEventListener('click', function () { displayed_img[0].setAttribute('src', 'images/pic5.jpg'); });
+
+
+
+
 
 
 /* Wiring up the Darken/Lighten button */
